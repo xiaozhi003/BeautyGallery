1e37214eed546a0504b40548d5e4caf4b0ae609e
xiaozhiwz <xiaozhiwz@163.com>
2015-05-13 10:45:24 +0800

    update

---
+++ src/com/xiaozhi/beautygallery/activity/BeautyActivity.java
@@ -0,0 +1,13 @@
+package com.xiaozhi.beautygallery.activity;
+
+import android.app.Fragment;
+
+public class BeautyActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ src/com/xiaozhi/beautygallery/fragment/BeautyFragment.java
@@ -0,0 +1,19 @@
+package com.xiaozhi.beautygallery.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BeautyFragment extends Fragment {
+	
+	public static final String URL = "url";
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
